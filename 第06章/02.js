41c6b7215cd14c0fbe8d7bf0e7ee31f388465a4a
liu <guangxuanliu@gmail.com>
2017-05-10 21:37:06 +0800

    存取器属性

---
+++ 第06章/02.js
@@ -0,0 +1,28 @@
+// var obj = {
+//     name : 'liu guangxuan',
+//     age : 25,
+//     's-ex' : 'male'
+// };
+// console.log(obj);
+// console.log('keys:' + Object.keys(obj));
+// console.log('names:' + Object.getOwnPropertyNames(obj));
+// for(prop in obj){
+//     console.log(prop + " : " + obj[prop]);
+// }
+// console.log(Object.getOwnPropertyNames(Object));
+// console.log(Object.keys(Object));
+// var serialnum = {
+//     $n : 0,
+//     get next(){
+//         return this.$n++;
+//     },
+//     set next(n){
+//         if(n > this.$n){
+//             this.$n = n;
+//         }
+//         else {
+//             throw '序列号的值不能比当前值小';
+//         }
+//     }
+// };
+

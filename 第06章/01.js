35357d5a20020447a07e796d421df5e47a281b84
liu <guangxuanliu@gmail.com>
2017-05-11 21:04:23 +0800

    Chapter 06

--- 第06章/01.js
+++ 第06章/01.js
@@ -13,3 +13,14 @@ Object.defineProperty(obj,'x',{
     value : 2
 });
 console.log(obj);
+
+console.info('6.8.1 原型属性');
+
+var o = Object.create(null);
+console.log(o);
+
+function classof(o) {
+    if (o === null) return 'Null';
+    if(o === undefined) return 'Undefined';
+    return Object.prototype.toString.call(o).slice(8,-1);
+}

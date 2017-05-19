47db2d97a415c0ab9edfc9016c4abd52634473bd
liu <guangxuanliu@gmail.com>
2017-05-19 20:44:18 +0800

    Chapter 09 构造函数

--- 第09章/main.js
+++ 第09章/main.js
@@ -1,2 +1,19 @@
 console.info('9.1 类和原型');
-
+function Range(from,to) {
+    this.from = from;
+    this.to = to;
+}
+Range.prototype = {
+    constructor : Range,
+    include : function (x) {
+        return x >= this.from && x <= this.to;
+    },
+    foreach: function (f) {
+        for(var x = Math.ceil(this.from);x <= this.to;x++)
+            f(x);
+    },
+    toString:function () {
+        return '(' + this.from + '...' + this.to + ')';
+    }
+};
+var range = new Range(1,3);
